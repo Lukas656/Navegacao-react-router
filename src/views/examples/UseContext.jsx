d400c88933d6bb989b2ccdba20393ef5cb1579fa
Lucas <Lukas656>
2023-09-23 13:00:06 -0300

    Aceita um objeto de contexto  retorna atual do contexto!

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import DataContext from '../../data/DataContext'
 
 const UseContext = (props) => {
+
+    const context = useContext(DataContext)
+
+    function addNumber(delta) {
+        context.setState({
+            ...context.state,  // Aqui usando os ... eu pego todos os dado do objeto para não ser alterado e a baixo eu declaro o valor que quero realmente alterar 
+            number: context.state.number + delta
+        })
+    }
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <div className='center'>
+                <span className='text'>{context.state.text}</span>
+                <span className='text'>{context.state.number}</span>
+            </div>
+            <div>
+                <button className='btn'onClick={()=>addNumber(-1)}>-1</button>
+                <button className='btn'onClick={()=>addNumber(+1)}>+1</button>
+            </div>
         </div>
     )
 }
