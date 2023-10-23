371fbc4a1ca80ea1a57f5de35dc66cb83bc70c4f
Lucas <Lukas656>
2023-10-23 20:08:21 -0300

    Criando um hook e alimentando api de dados json

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import { useCounter } from '../../hook/useCounter'
+import { useFetch } from '../../hook/usefetch'
 
 
 const UseRef = (props) => {
     const [count, inc, dec] = useCounter(10)
-
+const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+const response = useFetch(url)
+ function showStates(states){
+ return states.map(state => <li key={state.nome}>{state.nome} - {state.sigla}</li>)
+ }
     return (
         <div className="UseCustom">
             <PageTitle
@@ -20,6 +25,12 @@ const UseRef = (props) => {
                     <button className='btn' onClick={()=> dec()}>-1</button>
                     <button className='btn' onClick={()=> inc()}>+1</button>
                 </div>
+            </div>
+            <SectionTitle title= "Exercicio #02" />
+            <div className='center'>
+                <ul>
+                    {response.loading ? showStates(response.data) : false}
+                </ul>
             </div>
         </div>
     )
